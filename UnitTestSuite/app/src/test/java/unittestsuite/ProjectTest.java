59b2ea7296689c5593bd0e3e77d88a2e97510136
Steven Tian <zhenyuan.tian@mail.mcgill.ca>
2024-10-07 01:10:47 -0400

    add xml tests

--- UnitTestSuite/app/src/test/java/unittestsuite/ProjectTest.java
+++ UnitTestSuite/app/src/test/java/unittestsuite/ProjectTest.java
@@ -19,6 +19,11 @@ public class ProjectTest {
         "\"description\": \"Description of new project\"\n" +
         "}";
 
+    private static final String projectBodyXml = "<project>" +
+    "<title>New Project</title>" +
+    "<description>Description of new project</description>" +
+    "</project>";
+
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "http://localhost:4567/projects";
@@ -54,6 +59,55 @@ public class ProjectTest {
     }
 
     @Test
+    public void testGetAllProjects_XML() {
+        given()
+        .accept(ContentType.XML)
+        .when()
+            .get("")
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.XML)
+            .body(hasXPath("/projects/project"));
+    }
+
+    @Test
+    public void testPostProject_XML() {
+        String idString = given()
+        .body(projectBodyXml)
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)
+        .when()
+            .post("")
+        .then()
+            .statusCode(201)
+            .contentType(ContentType.XML)
+            .body(hasXPath("/project/id"))
+            .body(hasXPath("/project/title[text()='New Project']"))
+            .body(hasXPath("/project/description[text()='Description of new project']"))
+            .extract()
+            .path("project.id");
+
+        // Clean up - delete the created project
+        delete("/" + idString);
+    }
+
+    @Test
+    public void testPutProject_XML() {
+        // Assuming a valid project exists with id=5
+        given()
+        .body(projectBodyXml)
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)
+        .when()
+            .put("/5")
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.XML)
+            .body(hasXPath("/project/title[text()='New Project']"))
+            .body(hasXPath("/project/description[text()='Description of new project']"));
+    }
+
+    @Test
     public void testPostProjectInvalidFormat() {
         given()
         .when()
@@ -63,6 +117,27 @@ public class ProjectTest {
             .statusCode(400)
             .body("errorMessages", notNullValue());
     }
+
+    @Test
+    public void testPostProjectInvalidFormat_XML() {
+        String invalidXmlBody = "<projects>" +
+            "<project>" +
+            "<title>Invalid Project</title>" +
+            "<description>Invalid project format" +  // Missing closing tag for description
+            "</project>" +
+            "</projects>";
+
+        given()
+        .body(invalidXmlBody)
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)
+        .when()
+            .post("")
+        .then()
+            .statusCode(400)
+            .body("errorMessages", notNullValue());  // Check for error messages indicating bad format
+    }
+
 
     @Test
     public void testHeadAllProjects() {
@@ -196,6 +271,91 @@ public class ProjectTest {
     }
 
     @Test
+    public void testGetSpecificProject_XML() {
+        // Test valid project ID
+        given()
+        .accept(ContentType.XML)
+        .when()
+            .get("/5")
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.XML)
+            .body(hasXPath("/projects/project/id[text()='5']"))  // Check project ID
+            .body(hasXPath("/projects/project/title[text()='New Project']"))  // Check project title
+            .body(hasXPath("/projects/project/description[text()='Description of new project']"))  // Check project description
+            .body(hasXPath("/projects/project/completed[text()='false']"))  // Check project completed status
+            .body(hasXPath("/projects/project/active[text()='false']"));  // Check project active status
+
+        // Test invalid project ID
+        given()
+        .accept(ContentType.XML)
+        .when()
+            .get("/999")
+        .then()
+            .statusCode(404);  // No need to check body, just ensure 404 is returned
+    }
+
+    @Test
+    public void testPutSpecificProject_XML() {
+        String projectBodyXml = "<project>" +
+            "<title>New Project</title>" +
+            "<description>Description of new project</description>" +
+            "</project>";
+
+        // Test updating a valid project
+        given()
+        .body(projectBodyXml)
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)
+        .when()
+            .put("/5")
+        .then()
+            .statusCode(200)
+            .body(hasXPath("/project/title[text()='New Project']"))  // Check updated title
+            .body(hasXPath("/project/description[text()='Description of new project']"));  // Check updated description
+
+        // Test updating an invalid project ID
+        given()
+        .body(projectBodyXml)
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)
+        .when()
+            .put("/999")
+        .then()
+            .statusCode(404);  // No need to check body, just ensure 404 is returned
+    }
+
+    @Test
+    public void testPostToSpecificProject_XML() {
+        String projectBodyXml = "<project>" +
+            "<title>New Project</title>" +
+            "<description>Description of new project</description>" +
+            "</project>";
+
+        // Test posting to a specific project
+        given()
+        .body(projectBodyXml)
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)
+        .when()
+            .post("/5")
+        .then()
+            .statusCode(200)
+            .body(hasXPath("/project/title[text()='New Project']"))  // Check that the title is "New Project"
+            .body(hasXPath("/project/description[text()='Description of new project']"));  // Check description
+
+        // Test posting to a non-existent project
+        given()
+        .body(projectBodyXml)
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)
+        .when()
+            .post("/999")
+        .then()
+            .statusCode(404);  // No need to check body, just ensure 404 is returned
+    }
+
+    @Test
     public void testHeadForSpecificProject() {
         // Test HEAD request for an existing project
         given()
@@ -240,6 +400,23 @@ public class ProjectTest {
     }
 
     @Test
+    public void testGetAllTasksForProject_XML() {
+        // Test retrieving all tasks (todos) for a specific project in XML format
+        given()
+        .accept(ContentType.XML)  // Request XML format
+        .when()
+            .get("/3/tasks")
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.XML)  // Ensure the response is in XML format
+            .body(hasXPath("/todos/todo"))  // Ensure that there is at least one <todo> element
+            .body(hasXPath("/todos/todo/id"))  // Ensure that the first <todo> element has an <id>
+            .body(hasXPath("/todos/todo/title"))  // Ensure that the <todo> element has a <title>
+            .body(hasXPath("/todos/todo/doneStatus"));  // Ensure that the <todo> element has a <doneStatus>
+    }
+
+
+    @Test
     public void testHeadForTasksForProject() {
         // Test HEAD request for tasks related to a specific project
         given()
@@ -278,6 +455,48 @@ public class ProjectTest {
         .then()
             .statusCode(404)
             .body("errorMessages", notNullValue());  // Check for error messages in the response
+    }
+
+    @Test
+    public void testPostTaskRelationshipForProject_XML() {
+        // Test creating a relationship between a project and a task by specifying task details in XML
+        String requestBodyXml = "<task>" +
+            "<title>new work 3</title>" +
+            "<doneStatus>false</doneStatus>" +
+            "<description></description>" +
+            "<tasksof>" +
+                "<task><id>5</id></task>" +  // Related task id
+            "</tasksof>" +
+        "</task>";
+
+        given()
+        .when()
+            .body(requestBodyXml)  // Use XML format
+            .contentType(ContentType.XML)
+            .accept(ContentType.XML)  // Request response in XML
+            .post("/5/tasks")  // Assuming project with id=5 exists
+        .then()
+            .statusCode(201);
+
+        // Test invalid case (Bad Request 400) with incorrect task ID
+        String invalidRequestBodyXml = "<task>" +
+            "<title>new work 3</title>" +
+            "<doneStatus>false</doneStatus>" +
+            "<description></description>" +
+            "<tasksof>" +
+                "<task><id>invalid-id</id></task>" +  // Invalid task id
+            "</tasksof>" +
+        "</task>";
+
+        given()
+        .when()
+            .body(invalidRequestBodyXml)  // Use XML format for invalid request
+            .contentType(ContentType.XML)
+            .accept(ContentType.XML)
+            .post("/5/tasks")
+        .then()
+            .statusCode(404)
+            .body(hasXPath("/errorMessages"));  // Check for error messages in the response
     }
 
     @Test
@@ -346,6 +565,63 @@ public class ProjectTest {
         .then()
             .statusCode(404);
     }
+
+    @Test
+    public void testGetAllCategoriesForProject_XML() {
+        // Test retrieving all categories for a specific project in XML format
+        given()
+        .accept(ContentType.XML)  // Request XML format
+        .when()
+            .get("/5/categories")
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.XML)  // Ensure the content type is XML
+            .body(hasXPath("/categories/category"));  // Ensure that at least one <category> is returned
+    }
+
+    @Test
+    public void testHeadForCategoriesForProject_XML() {
+        // Test HEAD request for categories related to a specific project in XML format
+        given()
+        .accept(ContentType.XML)  // Request XML format
+        .when()
+            .head("/5/categories")
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.XML);  // Ensure the content type is XML
+    }
+
+    @Test
+    public void testPostCategoryRelationshipForProject_XML() {
+        // Test creating a relationship between a project and a category by specifying category title in the body (XML)
+        String requestBodyXml = "<category>" +
+            "<title>new category</title>" +
+        "</category>";
+
+        given()
+        .body(requestBodyXml)  // Use XML format
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)  // Request response in XML
+        .when()
+            .post("/5/categories")
+        .then()
+            .statusCode(201);
+
+        // Test invalid case (Bad Request 400) with incorrect category ID
+        String invalidRequestBodyXml = "<category>" +
+            "<id>invalid-id</id>" +  // Invalid category id
+        "</category>";
+
+        given()
+        .body(invalidRequestBodyXml)  // Use XML format for invalid request
+        .contentType(ContentType.XML)
+        .accept(ContentType.XML)
+        .when()
+            .post("/5/categories")
+        .then()
+            .statusCode(404);
+    }
+
 
     @Test
     public void testDeleteCategory() {
