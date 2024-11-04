4ab926ca898bb77171e151c1efd289082da2618d
Brian Morava <zmorava515@hotmail.com>
2024-11-04 01:55:56 -0500

    step definition skeleton code for category feature files

--- StoryTestSuite/app/src/test/java/storytestsuite/category/CategoryStepDefinitions.java
+++ StoryTestSuite/app/src/test/java/storytestsuite/category/CategoryStepDefinitions.java
@@ -1,5 +1,129 @@
 package test.java.storytestsuite.category;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
 public class CategoryStepDefinitions {
-    
+
+    // Step definitions for "Create Category" feature
+
+    @When("the user tries to create a new category with title {string}, and description {string}")
+    public void createCategoryWithTitleAndDescription(String title, String description) {
+        // Code to create a category with the specified title and description
+    }
+
+    @Then("the category named {string} is successfully created with description {string}")
+    public void verifyCategoryCreatedWithDescription(String title, String description) {
+        // Code to verify that the category was created with the given title and description
+    }
+
+    @Then("the category named {string} can be found with description {string}")
+    public void verifyCategoryExistsWithDescription(String title, String description) {
+        // Code to confirm that the category exists with the specified title and description
+    }
+
+    @When("the user tries to create a new category with title {string}")
+    public void createCategoryWithTitleOnly(String title) {
+        // Code to create a category with only the title
+    }
+
+    @Then("the category named {string} is successfully created with an empty description")
+    public void verifyCategoryCreatedWithEmptyDescription(String title) {
+        // Code to verify that the category was created with an empty description
+    }
+
+    @When("the user tries to create a new category with description {string}")
+    public void createCategoryWithDescriptionOnly(String description) {
+        // Code to attempt creating a category with only a description
+    }
+
+    @Then("the new category is not created")
+    public void verifyCategoryNotCreated() {
+        // Code to verify that the category was not created
+    }
+
+    // Step definitions for "Get Category List" feature
+
+    @Given("the categories with the following titles:")
+    public void givenCategoriesWithTitles(io.cucumber.datatable.DataTable titles) {
+        // Code to set up categories with given titles in the system
+    }
+
+    @When("the user tries to get a list of all categories")
+    public void getAllCategories() {
+        // Code to retrieve all categories
+    }
+
+    @Then("the categories with the following titles are found:")
+    public void verifyCategoriesFound(io.cucumber.datatable.DataTable titles) {
+        // Code to verify that the specified categories are returned in the list
+    }
+
+    @Given("the categories with the following titles and descriptions exist:")
+    public void givenCategoriesWithTitlesAndDescriptions(io.cucumber.datatable.DataTable titlesAndDescriptions) {
+        // Code to set up categories with titles and descriptions
+    }
+
+    @When("the user tries to get a list of categories with descriptions of {string}")
+    public void getCategoriesWithDescription(String description) {
+        // Code to retrieve categories matching the specified description
+    }
+
+    @Given("the category named {string} exists")
+    public void givenCategoryExists(String title) {
+        // Code to ensure a category with the specified title exists
+    }
+
+    @When("the user tries to get the list of categories with non-existent property {string} set to {string}")
+    public void getCategoriesWithNonExistentProperty(String property, String value) {
+        // Code to attempt retrieval of categories based on a non-existent property
+    }
+
+    @Then("no category is found")
+    public void verifyNoCategoryFound() {
+        // Code to verify that no categories are found
+    }
+
+    // Step definitions for "Update Category" feature
+
+    @Given("the category named {string} exists with description {string}")
+    public void givenCategoryExistsWithDescription(String title, String description) {
+        // Code to ensure a category with specified title and description exists
+    }
+
+    @When("the user tries to update the description of the category named {string} to {string}")
+    public void updateCategoryDescription(String title, String newDescription) {
+        // Code to update the category's description
+    }
+
+    @Then("the category is successfully updated with description {string}")
+    public void verifyCategoryUpdatedWithDescription(String newDescription) {
+        // Code to verify the category was updated with the new description
+    }
+
+    @When("the user tries to update the category named {string} to {string}")
+    public void updateCategoryTitle(String title, String newTitle) {
+        // Code to update the category's title
+    }
+
+    @Then("the category named {string} can be found")
+    public void verifyCategoryCanBeFound(String title) {
+        // Code to verify that the category exists with the specified title
+    }
+
+    @Then("the category named {string} cannot be found")
+    public void verifyCategoryCannotBeFound(String title) {
+        // Code to verify that a category with the old title no longer exists
+    }
+
+    @When("the user tries to update the category named {string} to id {string}")
+    public void updateCategoryId(String title, String newId) {
+        // Code to attempt updating the category's ID, which should fail
+    }
+
+    @Then("the category is not updated")
+    public void verifyCategoryNotUpdated() {
+        // Code to confirm the category was not updated
+    }
 }
